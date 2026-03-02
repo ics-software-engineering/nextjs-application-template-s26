f4a51abd951b05c31c8c3da0b86937b23707babb
Cam Moore <cammymoore07@gmail.com>
2026-03-02 09:59:26 -1000

    Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,21 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { Col, Container, Image, Row } from 'react-bootstrap';
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
-}
+/** The Home page. */
+const Home = () => (
+  <main>
+    <Container id="landing-page" fluid className="py-3">
+      <Row className="align-middle text-center">
+        <Col xs={4}>
+          <Image src="next.svg" width="150px" alt="" />
+        </Col>
+
+        <Col xs={8} className="d-flex flex-column justify-content-center">
+          <h1>Welcome to this template</h1>
+          <p>Now get to work and modify this app!</p>
+        </Col>
+      </Row>
+    </Container>
+  </main>
+);
+
+export default Home;
